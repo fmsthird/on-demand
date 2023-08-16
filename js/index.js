029a3d49241de2d2452f30ace5efb2cab5233dcc
Jhonas Lalo <titojonesisreal@gmail.com>
2023-08-16 19:45:44 +0800

    added one modal teksten and do js for toggle to show and hide modals

---
+++ js/index.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toolbarListBtns = document.querySelectorAll(".--list-container");
+
+  toolbarListBtns.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      const toggleTarget = button.getAttribute("data-toggle-target");
+      const targetContainer = document.querySelector(`.${toggleTarget}`);
+
+      if (targetContainer) {
+        toolbarListBtns.forEach((otherButton) => {
+          if (otherButton !== button) {
+            const otherToggleTarget =
+              otherButton.getAttribute("data-toggle-target");
+            const otherTargetContainer = document.querySelector(
+              `.${otherToggleTarget}`
+            );
+
+            if (otherTargetContainer) {
+              otherButton.classList.remove("active");
+              otherTargetContainer.style.display = "";
+            }
+          }
+        });
+
+        button.classList.toggle("active");
+        targetContainer.style.display =
+          targetContainer.style.display === "flex" ? "" : "flex";
+      }
+    });
+  });
+});
