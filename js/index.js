31c0313816a161ba8c568200322886570bd42478
Jhonas Lalo <titojonesisreal@gmail.com>
2023-08-17 11:49:12 +0800

    added indeling modal part

--- js/index.js
+++ js/index.js
@@ -1,33 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toolbarListBtns = document.querySelectorAll(".--list-container");
+const toolbarListBtns = document.querySelectorAll(".--list-container");
 
-  toolbarListBtns.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      event.preventDefault();
+toolbarListBtns.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
 
-      const toggleTarget = button.getAttribute("data-toggle-target");
-      const targetContainer = document.querySelector(`.${toggleTarget}`);
+    const toggleTarget = button.getAttribute("data-toggle-target");
+    const targetContainer = document.querySelector(`.${toggleTarget}`);
 
-      if (targetContainer) {
-        toolbarListBtns.forEach((otherButton) => {
-          if (otherButton !== button) {
-            const otherToggleTarget =
-              otherButton.getAttribute("data-toggle-target");
-            const otherTargetContainer = document.querySelector(
-              `.${otherToggleTarget}`
-            );
+    if (targetContainer) {
+      toolbarListBtns.forEach((otherButton) => {
+        if (otherButton !== button) {
+          const otherToggleTarget =
+            otherButton.getAttribute("data-toggle-target");
+          const otherTargetContainer = document.querySelector(
+            `.${otherToggleTarget}`
+          );
 
-            if (otherTargetContainer) {
-              otherButton.classList.remove("active");
-              otherTargetContainer.style.display = "";
-            }
+          if (otherTargetContainer) {
+            otherButton.classList.remove("active");
+            otherTargetContainer.style.display = "";
           }
-        });
+        }
+      });
 
-        button.classList.toggle("active");
-        targetContainer.style.display =
-          targetContainer.style.display === "flex" ? "" : "flex";
-      }
-    });
+      button.classList.toggle("active");
+      targetContainer.style.display =
+        targetContainer.style.display === "flex" ? "" : "flex";
+    }
   });
 });
